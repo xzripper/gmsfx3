f779fc7702fb3964953eca6ca2f71fd3eb4a16c2
Ivan <94743980+xzripper@users.noreply.github.com>
2024-05-06 22:25:15 +0300

    Python GMSFX3.
    Python GMSFX3.

---
+++ pygmsfx3/__init__.py
@@ -0,0 +1,46 @@
+"""Python GMSFX3 Wrapper."""
+
+from pywebloader import PyLoader
+
+from requests import get
+
+from warnings import catch_warnings
+
+from typing import Union
+
+
+GMSFX3_VERSION: str = 'v1.0.0'
+
+SOUNDS_BASE_VERSION_URL: str = 'https://raw.githubusercontent.com/xzripper/gmsfx3-sounds/main/sounds/BASE-VERSION'
+
+SFXName = str
+"""SFX Name."""
+
+GMSFX3DownloadResult = Union[str, tuple[str, str]]
+"""`gmsfx3_download` return."""
+
+def gmsfx3_sfx_url(sfxname: SFXName) -> str:
+    """Generate SFX URL."""
+    return f'https://github.com/xzripper/gmsfx3-sounds/blob/main/sounds/{sfxname}.wav?raw=true'
+
+def gmsfx3_download(sfxname: SFXName) -> GMSFX3DownloadResult:
+    """Download SFX."""
+    with catch_warnings(record=True) as warning:
+        webfile = PyLoader.webfile(gmsfx3_sfx_url(sfxname), 'wav')
+
+        if warning:
+            return (f'Failed to download SFX: `{sfxname}`.', sfxname)
+
+    return webfile[0]
+
+def gmsfx3_clear_cached_sfx() -> None:
+    """Clear cached SFX."""
+    PyLoader.clear_temp_files()
+
+def gmsfx3_version() -> str:
+    """Get GMSFX3 version."""
+    return GMSFX3_VERSION
+
+def gmsfx3_sounds_base_version() -> str:
+    """Get sounds base version."""
+    return get(SOUNDS_BASE_VERSION_URL).content.decode()[:-1]
---
+++ pygmsfx3/sfxlist.py
@@ -0,0 +1,303 @@
+"""SFX List."""
+
+from . import SFXName
+
+
+class GMSFX3:
+    """SFX List."""
+
+    class Ambiance:
+        class Dark:
+            AMBIENCE_DARK_1: SFXName = 'ambience_dark_1'
+            AMBIENCE_DARK_2: SFXName = 'ambience_dark_2'
+            AMBIENCE_DARK_3: SFXName = 'ambience_dark_3'
+            AMBIENCE_DARK_4: SFXName = 'ambience_dark_4'
+            AMBIENCE_DARK_5: SFXName = 'ambience_dark_5'
+
+        class FarHowlingWind:
+            AMBIENCE_FAR_HOWLING_WIND_1: SFXName = 'ambience_far_howling_wind_1'
+            AMBIENCE_FAR_HOWLING_WIND_2: SFXName = 'ambience_far_howling_wind_2'
+            AMBIENCE_FAR_HOWLING_WIND_3: SFXName = 'ambience_far_howling_wind_3'
+            AMBIENCE_FAR_HOWLING_WIND_4: SFXName = 'ambience_far_howling_wind_4'
+            AMBIENCE_FAR_HOWLING_WIND_5: SFXName = 'ambience_far_howling_wind_5'
+
+        class Forest:
+            AMBIENCE_FOREST_1: SFXName = 'ambience_forest_1'
+            AMBIENCE_FOREST_2: SFXName = 'ambience_forest_2'
+            AMBIENCE_FOREST_3: SFXName = 'ambience_forest_3'
+            AMBIENCE_FOREST_4: SFXName = 'ambience_forest_4'
+            AMBIENCE_FOREST_5: SFXName = 'ambience_forest_5'
+
+        class HowlingWind:
+            AMBIENCE_HOWLING_WIND_1: SFXName = 'ambience_howling_wind_1'
+            AMBIENCE_HOWLING_WIND_2: SFXName = 'ambience_howling_wind_2'
+
+        class Night:
+            AMBIENCE_NIGHT_1: SFXName = 'ambience_night_1'
+            AMBIENCE_NIGHT_2: SFXName = 'ambience_night_2'
+
+    class Beep:
+        BEEP_1: SFXName = 'beep_1'
+        BEEP_2: SFXName = 'beep_2'
+        BEEP_3: SFXName = 'beep_3'
+
+    class Breathe:
+        BREATHE_1: SFXName = 'breathe_1'
+
+    class Closing:
+        class Closet:
+            CLOSING_CLOSET_1: SFXName = 'closing_closet_1'
+
+        class Door:
+            CLOSING_DOOR_1: SFXName = 'closing_door_1'
+
+        class Lattice:
+            CLOSING_LATTICE_1: SFXName = 'closing_lattice_1'
+
+        class Shelf:
+            CLOSING_SHELF_1: SFXName = 'closing_shelf_1'
+
+    class Crow:
+        CROW_1: SFXName = 'crow_1'
+        CROW_2: SFXName = 'crow_2'
+        CROW_3: SFXName = 'crow_3'
+        CROW_4: SFXName = 'crow_4'
+
+    class Crowd:
+        CROWD_1: SFXName = 'crowd_1'
+        CROWD_2: SFXName = 'crowd_2'
+
+    class Ding:
+        DING_1: SFXName = 'ding_1'
+
+    class Explosion:
+        class Big:
+            EXPLOSION_BIG_1: SFXName = 'explosion_big_1'
+            EXPLOSION_BIG_2: SFXName = 'explosion_big_2'
+            EXPLOSION_BIG_3: SFXName = 'explosion_big_3'
+            EXPLOSION_BIG_4: SFXName = 'explosion_big_4'
+            EXPLOSION_BIG_5: SFXName = 'explosion_big_5'
+
+        class Small:
+            EXPLOSION_SMALL_1: SFXName = 'explosion_small_1'
+            EXPLOSION_SMALL_2: SFXName = 'explosion_small_2'
+            EXPLOSION_SMALL_3: SFXName = 'explosion_small_3'
+            EXPLOSION_SMALL_4: SFXName = 'explosion_small_4'
+
+    class FallenObject:
+        class GlassVase:
+            FALLEN_OBJECT_GLASS_VASE_1: SFXName = 'fallen_object_glass_vase_1'
+
+        class HumanBody:
+            FALLEN_OBJECT_HUMAN_BODY_1: SFXName = 'fallen_object_human_body_1'
+            FALLEN_OBJECT_HUMAN_BODY_2: SFXName = 'fallen_object_human_body_2'
+            FALLEN_OBJECT_HUMAN_BODY_3: SFXName = 'fallen_object_human_body_3'
+
+        class MetalThing:
+            FALLEN_OBJECT_METAL_THING_1: SFXName = 'fallen_object_metal_thing_1'
+            FALLEN_OBJECT_METAL_THING_2: SFXName = 'fallen_object_metal_thing_2'
+
+        class Plank:
+            FALLEN_OBJECT_PLANK_1: SFXName = 'fallen_object_plank_1'
+
+        class Wood:
+            FALLEN_OBJECT_WOOD_1: SFXName = 'fallen_object_wood_1'
+            FALLEN_OBJECT_WOOD_2: SFXName = 'fallen_object_wood_2'
+
+    class FarHeliFlyBy:
+        FAR_HELI_FLY_BY_1: SFXName = 'far_heli_fly_by_1'
+
+    class FarJetFlyBy:
+        FAR_JET_FLY_BY_1: SFXName = 'far_jet_fly_by_1'
+        FAR_JET_FLY_BY_2: SFXName = 'far_jet_fly_by_2'
+        FAR_JET_FLY_BY_3: SFXName = 'far_jet_fly_by_3'
+        FAR_JET_FLY_BY_4: SFXName = 'far_jet_fly_by_4'
+
+    class FemaleHit:
+        FEMALE_HIT_1: SFXName = 'female_hit_1'
+        FEMALE_HIT_2: SFXName = 'female_hit_2'
+
+    class FemaleOuch:
+        FEMALE_OUCH_1: SFXName = 'female_ouch_1'
+        FEMALE_OUCH_2: SFXName = 'female_ouch_2'
+
+    class FireBoom:
+        FIRE_BOOM_1: SFXName = 'fire_boom_1'
+        FIRE_BOOM_2: SFXName = 'fire_boom_2'
+        FIRE_BOOM_3: SFXName = 'fire_boom_3'
+        FIRE_BOOM_4: SFXName = 'fire_boom_4'
+
+    class Footstep:
+        class Branch:
+            FOOTSTEP_BRANCH_1: SFXName = 'footstep_branch_1'
+            FOOTSTEP_BRANCH_2: SFXName = 'footstep_branch_2'
+            FOOTSTEP_BRANCH_3: SFXName = 'footstep_branch_3'
+            FOOTSTEP_BRANCH_4: SFXName = 'footstep_branch_4'
+
+        class Concrete:
+            FOOTSTEP_CONCRETE_1: SFXName = 'footstep_concrete_1'
+            FOOTSTEP_CONCRETE_2: SFXName = 'footstep_concrete_2'
+            FOOTSTEP_CONCRETE_3: SFXName = 'footstep_concrete_3'
+
+        class Grass:
+            FOOTSTEP_GRASS_1: SFXName = 'footstep_grass_1'
+            FOOTSTEP_GRASS_2: SFXName = 'footstep_grass_2'
+
+        class Gravel:
+            FOOTSTEP_GRAVEL_1: SFXName = 'footstep_gravel_1'
+            FOOTSTEP_GRAVEL_2: SFXName = 'footstep_gravel_2'
+            FOOTSTEP_GRAVEL_3: SFXName = 'footstep_gravel_3'
+            FOOTSTEP_GRAVEL_4: SFXName = 'footstep_gravel_4'
+
+        class Sand:
+            FOOTSTEP_SAND_1: SFXName = 'footstep_sand_1'
+            FOOTSTEP_SAND_2: SFXName = 'footstep_sand_2'
+
+        class Water:
+            FOOTSTEP_WATER_1: SFXName = 'footstep_water_1'
+
+        class Wood:
+            FOOTSTEP_WOOD_1: SFXName = 'footstep_wood_1'
+            FOOTSTEP_WOOD_2: SFXName = 'footstep_wood_2'
+
+    class GlassBreaks:
+        GLASS_BREAKS_1: SFXName = 'glass_breaks_1'
+        GLASS_BREAKS_2: SFXName = 'glass_breaks_2'
+        GLASS_BREAKS_3: SFXName = 'glass_breaks_3'
+        GLASS_BREAKS_4: SFXName = 'glass_breaks_4'
+
+    class GMSFX3Intro:
+        GMSFX3_INTRO_1: SFXName = 'gmsfx3_intro_1'
+        GMSFX3_INTRO_2: SFXName = 'gmsfx3_intro_2'
+        GMSFX3_INTRO_3: SFXName = 'gmsfx3_intro_3'
+
+    class Gun:
+        class Blaster:
+            GUN_BLASTER_SHOT_1: SFXName = 'gun_blaster_shot_1'
+            GUN_BLASTER_SHOT_2: SFXName = 'gun_blaster_shot_2'
+            GUN_BLASTER_SHOT_3: SFXName = 'gun_blaster_shot_3'
+            GUN_BLASTER_SHOT_4: SFXName = 'gun_blaster_shot_4'
+
+        class Pistol:
+            GUN_PISTOL_SHOT_1: SFXName = 'gun_pistol_shot_1'
+            GUN_PISTOL_SHOT_2: SFXName = 'gun_pistol_shot_2'
+            GUN_PISTOL_SHOT_3: SFXName = 'gun_pistol_shot_3'
+            GUN_PISTOL_SHOT_4: SFXName = 'gun_pistol_shot_4'
+
+        class Rifle:
+            GUN_RIFLE_SHOT_1: SFXName = 'gun_rifle_shot_1'
+            GUN_RIFLE_SHOT_2: SFXName = 'gun_rifle_shot_2'
+            GUN_RIFLE_SHOT_3: SFXName = 'gun_rifle_shot_3'
+
+    class Human:
+        HUMAN_BREATHING_1: SFXName = 'human_breathing_1'
+
+    class Knife:
+        KNIFE_BLADE_1: SFXName = 'knife_blade_1'
+
+    class MaleHit:
+        MALE_HIT_1: SFXName = 'male_hit_1'
+        MALE_HIT_2: SFXName = 'male_hit_2'
+
+    class MaleOuch:
+        MALE_OUCH_1: SFXName = 'male_ouch_1'
+        MALE_OUCH_2: SFXName = 'male_ouch_2'
+
+    class Mechanical:
+        class Beep:
+            MECHANICAL_BEEP_1: SFXName = 'mechanical_beep_1'
+            MECHANICAL_BEEP_2: SFXName = 'mechanical_beep_2'
+
+        class Success:
+            MECHANICAL_SUCCESS_1: SFXName = 'mechanical_success_1'
+
+    class Meow:
+        MEOW_1: SFXName = 'meow_1'
+
+    class Opening:
+        class ClosenDoor:
+            OPENING_CLOSEN_DOOR_1: SFXName = 'opening_closen_door_1'
+
+        class Closet:
+            OPENING_CLOSET_1: SFXName = 'opening_closet_1'
+            OPENING_CLOSET_2: SFXName = 'opening_closet_2'
+
+        class Door:
+            OPENING_DOOR_1: SFXName = 'opening_door_1'
+            OPENING_DOOR_2: SFXName = 'opening_door_2'
+
+        class Shelf:
+            OPENING_SHELF_1: SFXName = 'opening_shelf_1'
+
+    class PoliceSiren:
+        class Far:
+            POLICE_SIREN_FAR_1: SFXName = 'police_siren_far_1'
+
+        class NearFar:
+            POLICE_SIREN_NEAR_FAR_1: SFXName = 'police_siren_near_far_1'
+
+        class Stop:
+            POLICE_SIREN_STOP_1: SFXName = 'police_siren_stop_1'
+
+    class PoweredGMSFX3:
+        POWERED_GMSFX3: SFXName = 'powered_gmsfx3'
+
+    class SeaAmbience:
+        SEA_AMBIENCE_1: SFXName = 'sea_ambience_1'
+        SEA_AMBIENCE_2: SFXName = 'sea_ambience_2'
+        SEA_AMBIENCE_3: SFXName = 'sea_ambience_3'
+        SEA_AMBIENCE_4: SFXName = 'sea_ambience_4'
+        SEA_AMBIENCE_5: SFXName = 'sea_ambience_5'
+
+    class Sirens:
+        SIRENS_1: SFXName = 'sirens_1'
+
+        class Ambient:
+            SIRENS_AMBIENT_1: SFXName = 'sirens_ambient_1'
+            SIRENS_AMBIENT_2: SFXName = 'sirens_ambient_2'
+
+        class Chaos:
+            SIRENS_CHAOS_1: SFXName = 'sirens_chaos_1'
+
+        class GettingFar:
+            SIRENS_GETTING_FAR_1: SFXName = 'sirens_getting_far_1'
+
+    class Teleport:
+        TELEPORT_1: SFXName = 'teleport_1'
+        TELEPORT_2: SFXName = 'teleport_2'
+
+    class Train:
+        class Horn:
+            TRAIN_HORN_1: SFXName = 'train_horn_1'
+            TRAIN_HORN_2: SFXName = 'train_horn_2'
+            TRAIN_HORN_3: SFXName = 'train_horn_3'
+
+    class TreesAffectedByWind:
+        TREES_AFFECTED_BY_WIND_1: SFXName = 'trees_affected_by_wind_1'
+
+    class Vehicle:
+        class Door:
+            class Close:
+                VEHICLE_DOOR_CLOSE_1: SFXName = 'vehicle_door_close_1'
+                VEHICLE_DOOR_CLOSE_2: SFXName = 'vehicle_door_close_2'
+
+            class Open:
+                VEHICLE_DOOR_OPEN_1: SFXName = 'vehicle_door_open_1'
+                VEHICLE_DOOR_OPEN_2: SFXName = 'vehicle_door_open_2'
+
+        class Driving:
+            VEHICLE_DRIVING_INSIDE_1: SFXName = 'vehicle_driving_inside_1'
+
+        class Engine:
+            class Running:
+                class Inside:
+                    VEHICLE_ENGINE_RUNNING_INSIDE_1: SFXName = 'vehicle_engine_running_inside_1'
+
+                class Outside:
+                    VEHICLE_ENGINE_RUNNING_OUTSIDE_1: SFXName = 'vehicle_engine_running_outside_1'
+
+            class Start:
+                VEHICLE_ENGINE_START_1: SFXName = 'vehicle_engine_start_1'
+
+        class PassingBy:
+            VEHICLE_PASSING_BY_1: SFXName = 'vehicle_passing_by_1'
