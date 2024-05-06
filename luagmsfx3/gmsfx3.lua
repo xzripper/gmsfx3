fb094590f72bf8a5e2ab73176252311c763923d6
Ivan <94743980+xzripper@users.noreply.github.com>
2024-05-07 00:45:05 +0300

    Lua Wrapper.
    Lua Wrapper.

---
+++ luagmsfx3/gmsfx3.lua
@@ -0,0 +1,58 @@
+-- Lua GMSFX3 Wrapper.
+
+---@alias SFXName string
+---@alias GMSFX3DownloadResult string | table
+
+--- GMSFX3 Version.
+---@type string
+GMSFX3_VERSION = "1.0.0"
+
+--- Sounds Base Version URL.
+---@type string
+SOUNDS_BASE_VERSION_URL = "https://raw.githubusercontent.com/xzripper/gmsfx3-sounds/main/sounds/BASE-VERSION"
+
+--- Generate SFX URL.
+---@param sfxname SFXName
+---@return string
+function gmsfx3_sfx_url(sfxname)
+    return string.format("https://github.com/xzripper/gmsfx3-sounds/blob/main/sounds/%s.wav?raw=true", sfxname)
+end
+
+--- Get SFX.
+---@param sfxname SFXName
+---@return GMSFX3DownloadResult
+function gmsfx3_get(sfxname)
+    local temporaryDirectory = os.getenv("TMPDIR") or os.getenv("TEMP") or os.getenv("TMP") or "/tmp"
+
+    local tempDirLastChar_ = string.sub(temporaryDirectory, #temporaryDirectory, #temporaryDirectory)
+
+    local pathSplit = ""
+
+    if tempDirLastChar_ ~= "\\" or tempDirLastChar_ ~= "/" then
+        pathSplit = package.config:sub(1, 1)
+    end
+
+    local outPath = string.format("%s%s%s_%s.wav", temporaryDirectory,  pathSplit, sfxname, math.random(1000, 9999))
+
+    local code = tonumber(io.popen(string.format("curl -s -L -o %s %s -w '%s{http_code}'", outPath, gmsfx3_sfx_url(sfxname), "%")):read("*a"):match("%d+"))
+
+    if code and code == 200 and code < 300 then
+        return outPath
+    else
+        return {string.format('Failed to download SFX: `%s`.', sfxname), sfxname}
+    end
+end
+
+--- Clear cached SFX. Not implemented in Lua (temporary).
+---@return nil
+function gmsfx3_clear_cached_sfx() end
+
+--- Get sounds base version. Not implemented in Lua (temporary).
+---@return nil
+function gmsfx3_sounds_base_version() end
+
+--- Get GMSFX3 version.
+---@return string
+function gmsfx3_version()
+    return GMSFX3_VERSION
+end
---
+++ luagmsfx3/sfxlist.lua
@@ -0,0 +1,384 @@
+-- SFX List.
+
+--- SFX List.
+local GMSFX3 = {}
+
+GMSFX3.Ambiance = {
+    Dark = {
+        AMBIENCE_DARK_1 = "ambience_dark_1",
+        AMBIENCE_DARK_2 = "ambience_dark_2",
+        AMBIENCE_DARK_3 = "ambience_dark_3",
+        AMBIENCE_DARK_4 = "ambience_dark_4",
+        AMBIENCE_DARK_5 = "ambience_dark_5"
+    },
+
+    FarHowlingWind = {
+        AMBIENCE_FAR_HOWLING_WIND_1 = "ambience_far_howling_wind_1",
+        AMBIENCE_FAR_HOWLING_WIND_2 = "ambience_far_howling_wind_2",
+        AMBIENCE_FAR_HOWLING_WIND_3 = "ambience_far_howling_wind_3",
+        AMBIENCE_FAR_HOWLING_WIND_4 = "ambience_far_howling_wind_4",
+        AMBIENCE_FAR_HOWLING_WIND_5 = "ambience_far_howling_wind_5"
+    },
+
+    Forest = {
+        AMBIENCE_FOREST_1 = "ambience_forest_1",
+        AMBIENCE_FOREST_2 = "ambience_forest_2",
+        AMBIENCE_FOREST_3 = "ambience_forest_3",
+        AMBIENCE_FOREST_4 = "ambience_forest_4",
+        AMBIENCE_FOREST_5 = "ambience_forest_5"
+    },
+
+    HowlingWind = {
+        AMBIENCE_HOWLING_WIND_1 = "ambience_howling_wind_1",
+        AMBIENCE_HOWLING_WIND_2 = "ambience_howling_wind_2"
+    },
+
+    Night = {
+        AMBIENCE_NIGHT_1 = "ambience_night_1",
+        AMBIENCE_NIGHT_2 = "ambience_night_2"
+    }
+}
+
+GMSFX3.Beep = {
+    BEEP_1 = "beep_1",
+    BEEP_2 = "beep_2",
+    BEEP_3 = "beep_3"
+}
+
+GMSFX3.Breathe = {
+    BREATHE_1 = "breathe_1"
+}
+
+GMSFX3.Closing = {
+    Closet = {
+        CLOSING_CLOSET_1 = "closing_closet_1"
+    },
+
+    Door = {
+        CLOSING_DOOR_1 = "closing_door_1"
+    },
+
+    Lattice = {
+        CLOSING_LATTICE_1 = "closing_lattice_1"
+    },
+
+    Shelf = {
+        CLOSING_SHELF_1 = "closing_shelf_1"
+    }
+}
+
+GMSFX3.Crow = {
+    CROW_1 = "crow_1",
+    CROW_2 = "crow_2",
+    CROW_3 = "crow_3",
+    CROW_4 = "crow_4"
+}
+
+GMSFX3.Crowd = {
+    CROWD_1 = "crowd_1",
+    CROWD_2 = "crowd_2"
+}
+
+GMSFX3.Ding = {
+    DING_1 = "ding_1"
+}
+
+GMSFX3.Explosion = {
+    Big = {
+        EXPLOSION_BIG_1 = "explosion_big_1",
+        EXPLOSION_BIG_2 = "explosion_big_2",
+        EXPLOSION_BIG_3 = "explosion_big_3",
+        EXPLOSION_BIG_4 = "explosion_big_4",
+        EXPLOSION_BIG_5 = "explosion_big_5"
+    },
+
+    Small = {
+        EXPLOSION_SMALL_1 = "explosion_small_1",
+        EXPLOSION_SMALL_2 = "explosion_small_2",
+        EXPLOSION_SMALL_3 = "explosion_small_3",
+        EXPLOSION_SMALL_4 = "explosion_small_4"
+    }
+}
+
+GMSFX3.FallenObject = {
+    GlassVase = {
+        FALLEN_OBJECT_GLASS_VASE_1 = "fallen_object_glass_vase_1"
+    },
+
+    HumanBody = {
+        FALLEN_OBJECT_HUMAN_BODY_1 = "fallen_object_human_body_1",
+        FALLEN_OBJECT_HUMAN_BODY_2 = "fallen_object_human_body_2",
+        FALLEN_OBJECT_HUMAN_BODY_3 = "fallen_object_human_body_3"
+    },
+
+    MetalThing = {
+        FALLEN_OBJECT_METAL_THING_1 = "fallen_object_metal_thing_1",
+        FALLEN_OBJECT_METAL_THING_2 = "fallen_object_metal_thing_2"
+    },
+
+    Plank = {
+        FALLEN_OBJECT_PLANK_1 = "fallen_object_plank_1"
+    },
+
+    Wood = {
+        FALLEN_OBJECT_WOOD_1 = "fallen_object_wood_1",
+        FALLEN_OBJECT_WOOD_2 = "fallen_object_wood_2"
+    }
+}
+
+GMSFX3.FarHeliFlyBy = {
+    FAR_HELI_FLY_BY_1 = "far_heli_fly_by_1"
+}
+
+GMSFX3.FarJetFlyBy = {
+    FAR_JET_FLY_BY_1 = "far_jet_fly_by_1",
+    FAR_JET_FLY_BY_2 = "far_jet_fly_by_2",
+    FAR_JET_FLY_BY_3 = "far_jet_fly_by_3",
+    FAR_JET_FLY_BY_4 = "far_jet_fly_by_4"
+}
+
+GMSFX3.FemaleHit = {
+    FEMALE_HIT_1 = "female_hit_1",
+    FEMALE_HIT_2 = "female_hit_2"
+}
+
+GMSFX3.FemaleOuch = {
+    FEMALE_OUCH_1 = "female_ouch_1",
+    FEMALE_OUCH_2 = "female_ouch_2"
+}
+
+GMSFX3.FireBoom = {
+    FIRE_BOOM_1 = "fire_boom_1",
+    FIRE_BOOM_2 = "fire_boom_2",
+    FIRE_BOOM_3 = "fire_boom_3",
+    FIRE_BOOM_4 = "fire_boom_4"
+}
+
+GMSFX3.Footstep = {
+    Branch = {
+        FOOTSTEP_BRANCH_1 = "footstep_branch_1",
+        FOOTSTEP_BRANCH_2 = "footstep_branch_2",
+        FOOTSTEP_BRANCH_3 = "footstep_branch_3",
+        FOOTSTEP_BRANCH_4 = "footstep_branch_4"
+    },
+
+    Concrete = {
+        FOOTSTEP_CONCRETE_1 = "footstep_concrete_1",
+        FOOTSTEP_CONCRETE_2 = "footstep_concrete_2",
+        FOOTSTEP_CONCRETE_3 = "footstep_concrete_3"
+    },
+
+    Grass = {
+        FOOTSTEP_GRASS_1 = "footstep_grass_1",
+        FOOTSTEP_GRASS_2 = "footstep_grass_2"
+    },
+
+    Gravel = {
+        FOOTSTEP_GRAVEL_1 = "footstep_gravel_1",
+        FOOTSTEP_GRAVEL_2 = "footstep_gravel_2",
+        FOOTSTEP_GRAVEL_3 = "footstep_gravel_3",
+        FOOTSTEP_GRAVEL_4 = "footstep_gravel_4"
+    },
+
+    Sand = {
+        FOOTSTEP_SAND_1 = "footstep_sand_1",
+        FOOTSTEP_SAND_2 = "footstep_sand_2"
+    },
+
+    Water = {
+        FOOTSTEP_WATER_1 = "footstep_water_1"
+    },
+
+    Wood = {
+        FOOTSTEP_WOOD_1 = "footstep_wood_1",
+        FOOTSTEP_WOOD_2 = "footstep_wood_2"
+    }
+}
+
+GMSFX3.GlassBreaks = {
+    GLASS_BREAKS_1 = "glass_breaks_1",
+    GLASS_BREAKS_2 = "glass_breaks_2",
+    GLASS_BREAKS_3 = "glass_breaks_3",
+    GLASS_BREAKS_4 = "glass_breaks_4"
+}
+
+GMSFX3.GMSFX3Intro = {
+    GMSFX3_INTRO_1 = "gmsfx3_intro_1",
+    GMSFX3_INTRO_2 = "gmsfx3_intro_2",
+    GMSFX3_INTRO_3 = "gmsfx3_intro_3"
+}
+
+GMSFX3.Gun = {
+    Blaster = {
+        GUN_BLASTER_SHOT_1 = "gun_blaster_shot_1",
+        GUN_BLASTER_SHOT_2 = "gun_blaster_shot_2",
+        GUN_BLASTER_SHOT_3 = "gun_blaster_shot_3",
+        GUN_BLASTER_SHOT_4 = "gun_blaster_shot_4"
+    },
+
+    Pistol = {
+        GUN_PISTOL_SHOT_1 = "gun_pistol_shot_1",
+        GUN_PISTOL_SHOT_2 = "gun_pistol_shot_2",
+        GUN_PISTOL_SHOT_3 = "gun_pistol_shot_3",
+        GUN_PISTOL_SHOT_4 = "gun_pistol_shot_4"
+    },
+
+    Rifle = {
+        GUN_RIFLE_SHOT_1 = "gun_rifle_shot_1",
+        GUN_RIFLE_SHOT_2 = "gun_rifle_shot_2",
+        GUN_RIFLE_SHOT_3 = "gun_rifle_shot_3"
+    }
+}
+
+GMSFX3.Human = {
+    HUMAN_BREATHING_1 = "human_breathing_1"
+}
+
+GMSFX3.Knife = {
+    KNIFE_BLADE_1 = "knife_blade_1"
+}
+
+GMSFX3.MaleHit = {
+    MALE_HIT_1 = "male_hit_1",
+    MALE_HIT_2 = "male_hit_2"
+}
+
+GMSFX3.MaleOuch = {
+    MALE_OUCH_1 = "male_ouch_1",
+    MALE_OUCH_2 = "male_ouch_2"
+}
+
+GMSFX3.Mechanical = {
+    Beep = {
+        MECHANICAL_BEEP_1 = "mechanical_beep_1",
+        MECHANICAL_BEEP_2 = "mechanical_beep_2"
+    },
+
+    Success = {
+        MECHANICAL_SUCCESS_1 = "mechanical_success_1"
+    }
+}
+
+GMSFX3.Meow = {
+    MEOW_1 = "meow_1"
+}
+
+GMSFX3.Opening = {
+    ClosenDoor = {
+        OPENING_CLOSEN_DOOR_1 = "opening_closen_door_1"
+    },
+
+    Closet = {
+        OPENING_CLOSET_1 = "opening_closet_1",
+        OPENING_CLOSET_2 = "opening_closet_2"
+    },
+
+    Door = {
+        OPENING_DOOR_1 = "opening_door_1",
+        OPENING_DOOR_2 = "opening_door_2"
+    },
+
+    Shelf = {
+        OPENING_SHELF_1 = "opening_shelf_1"
+    }
+}
+
+GMSFX3.PoliceSiren = {
+    Far = {
+        POLICE_SIREN_FAR_1 = "police_siren_far_1"
+    },
+
+    NearFar = {
+        POLICE_SIREN_NEAR_FAR_1 = "police_siren_near_far_1"
+    },
+
+    Stop = {
+        POLICE_SIREN_STOP_1 = "police_siren_stop_1"
+    }
+}
+
+GMSFX3.PoweredGMSFX3 = {
+    POWERED_GMSFX3 = "powered_gmsfx3"
+}
+
+GMSFX3.SeaAmbience = {
+    SEA_AMBIENCE_1 = "sea_ambience_1",
+    SEA_AMBIENCE_2 = "sea_ambience_2",
+    SEA_AMBIENCE_3 = "sea_ambience_3",
+    SEA_AMBIENCE_4 = "sea_ambience_4",
+    SEA_AMBIENCE_5 = "sea_ambience_5"
+}
+
+GMSFX3.Sirens = {
+    SIRENS_1 = "sirens_1",
+
+    Ambient = {
+        SIRENS_AMBIENT_1 = "sirens_ambient_1",
+        SIRENS_AMBIENT_2 = "sirens_ambient_2"
+    },
+
+    Chaos = {
+        SIRENS_CHAOS_1 = "sirens_chaos_1"
+    },
+
+    GettingFar = {
+        SIRENS_GETTING_FAR_1 = "sirens_getting_far_1"
+    }
+}
+
+GMSFX3.Teleport = {
+    TELEPORT_1 = "teleport_1",
+    TELEPORT_2 = "teleport_2"
+}
+
+GMSFX3.Train = {
+    Horn = {
+        TRAIN_HORN_1 = "train_horn_1",
+        TRAIN_HORN_2 = "train_horn_2",
+        TRAIN_HORN_3 = "train_horn_3"
+    }
+}
+
+GMSFX3.TreesAffectedByWind = {
+    TREES_AFFECTED_BY_WIND_1 = "trees_affected_by_wind_1"
+}
+
+GMSFX3.Vehicle = {
+    Door = {
+        Close = {
+            VEHICLE_DOOR_CLOSE_1 = "vehicle_door_close_1",
+            VEHICLE_DOOR_CLOSE_2 = "vehicle_door_close_2"
+        },
+
+        Open = {
+            VEHICLE_DOOR_OPEN_1 = "vehicle_door_open_1",
+            VEHICLE_DOOR_OPEN_2 = "vehicle_door_open_2"
+        }
+    },
+
+    Driving = {
+        VEHICLE_DRIVING_INSIDE_1 = "vehicle_driving_inside_1"
+    },
+
+    Engine = {
+        Running = {
+            Inside = {
+                VEHICLE_ENGINE_RUNNING_INSIDE_1 = "vehicle_engine_running_inside_1"
+            },
+
+            Outside = {
+                VEHICLE_ENGINE_RUNNING_OUTSIDE_1 = "vehicle_engine_running_outside_1"
+            }
+        },
+
+        Start = {
+            VEHICLE_ENGINE_START_1 = "vehicle_engine_start_1"
+        }
+    },
+
+    PassingBy = {
+        VEHICLE_PASSING_BY_1 = "vehicle_passing_by_1"
+    }
+}
+
+return GMSFX3
